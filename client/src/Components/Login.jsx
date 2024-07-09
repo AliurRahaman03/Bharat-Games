8dc481ef3659678cfacbc542bf727b3e241be680
Aliur Rahaman <106933055+Aliurgen@users.noreply.github.com>
2024-07-09 18:17:01 +0530

    .

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,8 +1,31 @@
+import {Link} from 'react-router-dom'
 
 export default function Login() {
+
+  function handleSubmit(){
+
+  }
+
+  function handleChange(){
+
+  }
+
   return (
     <>
-      
+      <section className="container">
+        <form className="form" onSubmit={(event)=>[
+          handleSubmit(event)
+        ]} action="">
+          <div>
+            <img src="" alt="" />
+            <h1>Login to Bharat-Games</h1>
+          </div>
+          <input className="inp" type="email" name="email" placeholder="E-mail" onChange={handleChange}/>
+          <input className="inp" type="password" name="password" placeholder="Password" onChange={handleChange}/>          
+          <button className="btn" type="submit">Login</button>
+          <p className="span-term">Don&apos;t Have any Account ?  <Link  to='/register'>Register</Link></p>
+        </form>
+      </section>
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -39,7 +39,6 @@ app.post('register',(req,res)=>{
             })
         }
     })
-
 })
 
 
